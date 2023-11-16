9155111e5e0f9009dd7322cf6a425900c1e33b78
Kampheta <kampheta@gmail.com>
2023-11-16 15:50:45 +0100

    Code from gpt

--- src/my/profiler/Slow.java
+++ src/my/profiler/Slow.java
@@ -1,28 +1,37 @@
 package my.profiler;
 
-public class Slow {
+import java.util.HashMap;
+import java.util.Map;
+
+public class ImprovedSlow {
+    private static Map<Long, Long> memo = new HashMap<>();
+
     public static void main(String[] args) {
         for (int i = 1; i < 50; i++) {
-            long start = System.currentTimeMillis();
-            System.out.println("Fibonacci recursive " + i + " = " + fibRecursive(i) + " took " + (System.currentTimeMillis() - start) + "ms");
-            start = System.currentTimeMillis();
-            System.out.println("Fibonacci fast      " + i + " = " + fibFast(i) + " took " + (System.currentTimeMillis() - start) + "ms");
+            measureAndPrint("Fibonacci recursive", i, ImprovedSlow::fibRecursive);
+            measureAndPrint("Fibonacci fast", i, ImprovedSlow::fibFast);
         }
     }
 
-
-
-    static long fibRecursive(long i) {
-        if (i < 2) return 1;
-        return fibRecursive(i-2) + fibRecursive(i -1);
+    private static void measureAndPrint(String label, long i, FibonacciFunction function) {
+        long start = System.nanoTime();
+        long result = function.calculate(i);
+        long duration = System.nanoTime() - start;
+        System.out.println(label + " " + i + " = " + result + " took " + duration + "ns");
     }
 
+    interface FibonacciFunction {
+        long calculate(long n);
+    }
 
+    private static long fibRecursive(long i) {
+        if (i < 2) return 1;
+        return memo.computeIfAbsent(i, key -> fibRecursive(key - 2) + fibRecursive(key - 1));
+    }
 
-
-    static long fibFast(long i) {
+    private static long fibFast(long i) {
         if (i < 2) return i;
-        int a = 0, b = 1, c = 0;
+        long a = 0, b = 1, c = 0;
         while (i-- > 0) {
             c = a + b;
             a = b;
